b419b2fa475aea3dce8ef546803fb8e951c4bce5
Tijs Moree <tijsmoree@gmail.com>
2018-08-11 20:33:33 +0200

    Changed to soundboard

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'tijsmoree'`, async(() => {
+  it(`should have as title 'soundboard'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('tijsmoree');
+    expect(app.title).toEqual('soundboard');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tijsmoree!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to soundboard!');
   }));
 });
