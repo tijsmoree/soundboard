3070134829171aa6aa51a9198ae3e691b6b1e011
Tijs Moree <tijsmoree@gmail.com>
2018-08-12 20:12:53 +0200

    Nicer filter pipe

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -5,11 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core'
 })
 export class FilterPipe implements PipeTransform {
 
-  transform (items: any[], field: string, filter: string): any {
+  transform (items: any[], filter: Object): any {
     if (!items || !filter) {
       return items
     }
 
-    return items.filter(item => item[field].toLowerCase().includes(filter.toLowerCase()))
+    for (const field of Object.keys(filter)) {
+      if (!filter[field]) {
+        continue
+      }
+      items = items.filter(item => item[field].toLowerCase().includes(filter[field].toLowerCase()))
+    }
+
+    return items
   }
 }
