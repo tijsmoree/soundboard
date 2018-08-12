6025662ab90346f2077b7d3fa624bd0738d57a4c
Tijs Moree <tijsmoree@gmail.com>
2018-08-12 23:20:16 +0200

    yii remove log

--- api/config/web.php
+++ api/config/web.php
@@ -5,7 +5,6 @@ $db = require(__DIR__ . '/db.php');
 return [
   'id' => 'yii2mini',
   'basePath' => dirname(__DIR__),
-  'bootstrap' => ['log'],
   'components' => [
     'request' => [
       'cookieValidationKey' => 'pfwappomgepaoemomcppjcp0w4054u9tqjfm2qqkjf'
@@ -15,15 +14,6 @@ return [
     ],
     'errorHandler' => [
       'errorAction' => 'site/error',
-    ],
-    'log' => [
-      'traceLevel' => YII_DEBUG ? 3 : 0,
-      'targets' => [
-        [
-          'class' => 'yii\log\FileTarget',
-          'levels' => ['error', 'warning']
-        ],
-      ],
     ],
     'db' => $db
   ]
