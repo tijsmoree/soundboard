4208f7352cbff2a54152fdae5245727ecc79c9c1
Tijs Moree <tijsmoree@gmail.com>
2018-08-13 00:39:23 +0200

    Fix urls in yii

--- api/config/web.php
+++ api/config/web.php
@@ -15,6 +15,11 @@ return [
     'errorHandler' => [
       'errorAction' => 'site/error',
     ],
+    'urlManager' => [
+      'class' => 'yii\web\UrlManager',
+      'enablePrettyUrl' => true,
+      'showScriptName' => false
+    ],
     'db' => $db
   ]
 ];
