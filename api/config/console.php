011f63fc7baac7d122d709a118c8e47c4b0b238f
Tijs Moree <tijsmoree@gmail.com>
2018-09-02 19:36:49 +0200

    Fixed bug

--- api/config/console.php
+++ api/config/console.php
@@ -3,19 +3,10 @@
 return [
     'id' => 'soundboard-tijsmoree',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
     'controllerNamespace' => 'app\commands',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
-        ],
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
         ],
         'db' => require(__DIR__ . '/db.php')
     ]
